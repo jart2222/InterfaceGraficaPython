7c93a2432e6a1b236228f277af93cf3ecb025a60
jart2222 <jart2607@gmail.com>
2021-12-18 20:16:58 -0600

    Primera version

---
+++ PrimeraInterface.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+raiz=Tk()
+raiz.title("Ventana de prubea")#nombre de la ventana
+raiz.resizable(True,True) #permitir width o heigt
+raiz.geometry("500x500")
+raiz.config(bg="black")
+raiz.mainloop()#crear ventana
