9ed6aba896dfb787cc16df806c3f746860859262
jart2222 <jart2607@gmail.com>
2021-12-18 22:15:39 -0600

    subi formato de la raiz y del frame

--- PrimeraInterface.py
+++ PrimeraInterface.py
@@ -3,6 +3,18 @@ from tkinter import *
 raiz=Tk()
 raiz.title("Ventana de prubea")#nombre de la ventana
 raiz.resizable(True,True) #permitir width o heigt
-raiz.geometry("500x500")
-raiz.config(bg="black")
+# raiz.geometry("500x500")
+# raiz.config(bg="black")
+miFrame=Frame()
+miFrame.pack() #metemos el frame en la ventana
+
+
+miFrame.config(bg="red",width="650",height="400",bd=30,relief="sunken",cursor="hand2")
+raiz.config(bg="blue",bd=30,relief="sunken")
+# miFrame.config(width="650",height="400")
+# miFrame.config(bd=30)
+# miFrame.config(relief="sunken")
+# miFrame.config(cursor="hand2")
+
+
 raiz.mainloop()#crear ventana
